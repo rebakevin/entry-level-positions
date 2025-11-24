a10a49fc6791eea209b1296d2337d5bbe6873219
Rebakure <kevinrebakure@gmail.com>
2025-11-24 22:14:29 +0200

    docs

--- src/scripts/config.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = '13a6f94d64msha3b555da25e50d1p1342e1jsn5cd3d2dbbd0c';
-export const API_HOST = 'jsearch.p.rapidapi.com';
-export const BASE_URL = 'https://jsearch.p.rapidapi.com/search';
